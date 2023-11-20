df7ae14a51974af772d13853c1e8c597fae964fb
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-20 19:20:55 +0530

    Create sum_of_two.java

---
+++ sum_of_two.java
@@ -0,0 +1,10 @@
+/*
+ * Sum of Two Numbers in Java
+ */
+public class sum_of_two {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum of Two Numbers is : "+a+b);
+    }
+}
