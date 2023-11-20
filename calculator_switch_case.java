113ea14c8422ee6408bd7acc304ece2a6cf91cea
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-20 19:52:34 +0530

    Create calculator_switch_case.java

---
+++ calculator_switch_case.java
@@ -0,0 +1,44 @@
+/*
+ * Calculator in Java using Switch Case
+ */
+
+import java.util.Scanner;
+
+public class calculator_switch_case {
+    public static void main(String[] args) {
+    Scanner val = new Scanner(System.in);
+    Scanner op = new Scanner(System.in); 
+
+        
+    System.out.println("Enter Value of X : ");
+    int x = val.nextInt();
+    System.out.println("Enter Value of Y : ");
+    int y = val.nextInt();
+
+    System.out.println("Enter a Operator :  * + / - ");
+    char operator = op.next().charAt(0);
+
+    switch (operator) {
+        case '+':
+        System.out.println("The Answer is : "+(x+y));
+            break;  
+        case '*':
+        System.out.println("The Answer is : "+(x*y));
+            break;  
+        case '/':
+        System.out.println("The Answer is : "+(x/y));
+            break;  
+        case '-':
+        System.out.println("The Answer is : "+(x-y));
+            break;  
+        default:
+        System.out.println("Invalid Values !");
+            break;
+    }
+    val.close();
+    op.close();
+    }
+        
+    }
+    
+
