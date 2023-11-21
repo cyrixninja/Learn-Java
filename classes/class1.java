555774bdcc62e8504f4f20d9b408bbddfbae808f
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-21 08:05:55 +0530

    Update

---
+++ classes/class1.java
@@ -0,0 +1,16 @@
+/*
+ * Example Code for Classes in Java
+ */
+
+package classes;
+
+
+public class class1 {
+    public static void main(String[] args) {
+        main myObj = new main();
+        System.out.println(myObj.x);
+        System.out.println(myObj.y);
+    }
+
+    
+}
---
+++ classes/main.java
@@ -0,0 +1,11 @@
+/*
+ * Example Code for Classes in Java
+ */
+
+package classes;
+
+public class main {
+    int x = 123;
+    int y =1234;
+
+}
