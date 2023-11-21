2304be5ffb7819a8e8643a63036fd664f06ca302
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-21 08:13:27 +0530

    update

---
+++ classes/class2.java
@@ -0,0 +1,19 @@
+/*
+ * Encapsulation in Java
+ */
+package classes;
+
+public class class2 {
+    private String name; // private = restricted access
+
+  // Getter
+  public String getName() {
+    return name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.name = newName;
+  }
+}
+
---
+++ classes/main2.java
@@ -0,0 +1,12 @@
+/*
+ * Encapsulation in Java
+ */
+package classes;
+
+public class main2 {
+    public static void main(String[] args) {
+        class2 myObj = new class2();
+     //   myObj.name = "John";  // error
+     // System.out.println(myObj.name); // error 
+      }
+}
