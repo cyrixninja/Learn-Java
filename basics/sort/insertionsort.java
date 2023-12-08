9db7fde4ca38fc89f6f7cebcefacb1f9bb4cf204
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-12-08 15:15:43 +0530

    Add Insertion Sort

---
+++ basics/sort/insertionsort.java
@@ -0,0 +1,27 @@
+package basics.sort;
+
+
+public class insertionsort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6, 3};
+        insertionSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
