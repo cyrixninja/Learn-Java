6e783fe53857a5f7e5f5a02be8a37f263fd80eaf
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-12-19 15:25:18 +0530

    Update sort algorithms in README.md

---
+++ basics/sort/timsort.java
@@ -0,0 +1,18 @@
+/*
+ * TimSort is a sorting algorithm based on Insertion Sort and Merge Sort.
+ * Here is the link for more information: https://en.wikipedia.org/wiki/Timsort
+ * Here's the Implementation of TimSort in Java:
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class timsort {
+    public static void main(String[] args) {
+        Integer[] arr = { 2, 5, 3, 7, 1, 9, 4, 6, 8 };
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+// Output: [9, 8, 7, 6, 5, 4, 3, 2, 1]
