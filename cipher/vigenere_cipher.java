f058ca4976ae4054952ee93615f5c5e886805385
cyrixninja <cyrixninja@outlook.com>
2023-12-27 22:53:41 +0530

    Add comments explaining Vigenere Cipher encryption algorithm

--- cipher/vigenere_cipher.java
+++ cipher/vigenere_cipher.java
@@ -1,4 +1,8 @@
 /*
+ * Vigenere Cipher is  a method of encrypting alphabetic text. 
+ * It uses a simple form of polyalphabetic substitution. 
+ * A polyalphabetic cipher is any cipher based on substitution, using multiple substitution alphabets .
+ * The encryption of the original text is done using the Vigenère square or Vigenère table.
  * Vigenere Cipher Encryption and Decryption Algorithm in Java
  */
 import java.util.Scanner;
